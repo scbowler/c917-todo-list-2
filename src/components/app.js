c9c8f840316da5a435f8e639244fbf031d46a34f
Scott Bowler <scott.bowler@learningfuze.com>
2017-11-08 11:59:22 -0800

    initial app and redux setup complete

--- src/components/app.js
+++ src/components/app.js
@@ -1,19 +1,9 @@
 import React from 'react';
-import './app.css';
-import logo from '../assets/images/logo.svg';
-import reactPumpkin from '../assets/images/october-react-logo-glow.png';
-import webs from '../assets/images/web-corner.png';
-import spider from '../assets/images/spider.png';
+import 'materialize-css/dist/css/materialize.min.css';
 
 const App = () => (
-    <div>
-        <div className="app">
-            <img src={reactPumpkin} className="logo rotate"/>
-            <h1>Welcome to React</h1>
-            <img src={webs} className="webs-r"/>
-            <img src={webs} className="webs-l"/>
-            <img src={spider} className="spider shake"/>
-        </div>
+    <div className="container">
+        <h1 className="center-align">To Do List 2.0</h1>
     </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers';
 
 import App from './components/app';
 
 ReactDOM.render(
-    <App />,
+    <Provider store={createStore(rootReducer, {}, applyMiddleware())}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
